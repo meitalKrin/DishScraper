4beb7eefa4d81761eb7c5aa64186c78d2cf70263
meitalKrin <km1234593@gmail.com>
2026-04-06 18:12:23 +0300

    first commit

--- app.py
+++ app.py
@@ -1,19 +1,35 @@
 from flask import Flask, render_template, request
-from bs4 import BeautifulSoup
-import requests
-app = Flask(__name__)
-@app.route('/',methods=['GET','POST'])
-def index():
-    if request.method == 'POST':
-        url_form_btn = request.form.get('recipe_url')
-        print({url_form_btn})
-        response = requests.get(url_form_btn)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        full_text = soup.get_text()
+from recipe_scrapers import scrape_html
+from curl_cffi import requests as crequests
 
-        return f"clicked url: {full_text}"
-    else: return render_template('templates.html')
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def get_recipe():
+    ingredients, instructions, error = None, None, None
+
+    if request.method == 'POST':
+        url = request.form.get('recipe_url')
+        try:
+
+            res = crequests.get(url, impersonate="chrome110", timeout=15)
+            res.raise_for_status()
+
+
+            scraper = scrape_html(html=res.text, org_url=url)
+
+            ingredients = scraper.ingredients()
+            instructions = scraper.instructions_list()
+
+            if not ingredients:
+                error = "Site loaded, but no recipe data could be found."
+
+
+        except Exception as e:
+            error = f"Error: {str(e)}"
+
+    return render_template("index.html", ingredients=ingredients, instructions=instructions, error=error)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
-
-
