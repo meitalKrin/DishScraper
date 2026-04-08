85abc2af2bec23eb50732f6fa0668439bde843df
meitalKrin <km1234593@gmail.com>
2026-04-08 21:55:37 +0300

    commit

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from flask import Flask, render_template, request
 from recipe_scrapers import scrape_html
 from curl_cffi import requests as crequests
+import pymongo
 
 app = Flask(__name__)
-
+try:
+    myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+    myDB = myclient["MyFullDB"]
+    mycol = myDB["recipes"]
+    myclient.server_info()
+except Exception as e:
+    print(f"MongoDB Connection Error: {e}")
 @app.route('/', methods=['GET', 'POST'])
 def get_recipe():
     ingredients, instructions, error = None, None, None
+
 
     if request.method == 'POST':
         url = request.form.get('recipe_url')
@@ -18,17 +26,33 @@ def get_recipe():
 
             scraper = scrape_html(html=res.text, org_url=url)
 
+
             ingredients = scraper.ingredients()
+            title  = scraper.title()
             instructions = scraper.instructions_list()
 
-            if not ingredients:
+
+            if not ingredients or not instructions :
                 error = "Site loaded, but no recipe data could be found."
+            else: x = mycol.insert_one({
+                "url": url,
+                "title": title,
+                "ingredients": ingredients,
+                "instructions": instructions
+            })
 
 
         except Exception as e:
             error = f"Error: {str(e)}"
 
-    return render_template("index.html", ingredients=ingredients, instructions=instructions, error=error)
+
+
+    return render_template("index.html", error=error)
+@app.route('/get_full_data', methods=['GET', 'POST'])
+def get_full_data():
+    titles= mycol.distinct("title")
+    return render_template("full_list.html",title=titles)
+
 
 
 if __name__ == '__main__':
